63b107459de37f5940461e62bed5214eb313f7f8
Sidakveer <sidakveersingh8@gmail.com>
2022-09-06 13:44:06 -0400

    making migrations and running them to generate tables for every model in a sqlite3 database

--- store/models.py
+++ store/models.py
@@ -16,8 +16,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -42,6 +43,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    class Meta:
+        db_table = "store_customers"
+        indexes = [
+            models.Index(fields=["last_name", "first_name"])
+        ]
+
 
 class Order(models.Model):
     PENDING = "P"
@@ -62,6 +69,7 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
