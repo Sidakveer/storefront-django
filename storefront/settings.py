60f2d4e899e414dc1557c8a8b7fc5376e5839489
Sidakveer <sidakveersingh8@gmail.com>
2022-09-08 13:49:39 -0400

    link our database with mysql 'datagrip to view' after sorting issues of pymysql and more

--- storefront/settings.py
+++ storefront/settings.py
@@ -1,3 +1,6 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
 """
 Django settings for storefront project.
 
@@ -87,8 +90,11 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "storefront",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "!Kartar11",
     }
 }
 
