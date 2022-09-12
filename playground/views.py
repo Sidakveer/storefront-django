ea869bf137b245c3660da5be7209c72a60d4dc21
Sidakveer <sidakveersingh8@gmail.com>
2022-09-12 13:50:24 -0400

    using querysets to filter different objects from our database

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,27 @@
+from operator import imod
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from store.models import Collection, Product, Customer, Order, OrderItem
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
+
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+    # queryset = Customer.objects.all()
+    # queryset = Product.objects.filter(inventory__lt=10)
+    # queryset = Customer.objects.filter(email__icontains=".com")
+    # queryset = Collection.objects.filter(featured_product__isnull=True)
+    # queryset = Order.objects.filter(customer__id=1)
+    queryset = OrderItem.objects.filter(order__customer__id=7)
+    
+
+
+
+
+    
+
+    return render(request, 'hello.html', {'name': 'Mosh', "products": list(queryset)})
