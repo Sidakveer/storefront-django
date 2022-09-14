ac4b7fe18c41a365a9fd56fd3ec127b5982f8c82
Sidakveer <sidakveersingh8@gmail.com>
2022-09-14 13:05:44 -0400

    using F object to link two different columns in a table

--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,7 @@ from operator import imod
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 
 from store.models import Collection, Product, Customer, Order, OrderItem
 
@@ -17,7 +17,7 @@ def say_hello(request):
     # queryset = Customer.objects.filter(email__icontains=".com")
     # queryset = Collection.objects.filter(featured_product__isnull=True)
     # queryset = Order.objects.filter(customer__id=1)
-    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F("collection__id"))
     
 
 
