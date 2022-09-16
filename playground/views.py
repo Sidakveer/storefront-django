65fe63a427282979ed6af92974e2af7cbeeb1a2b
Sidakveer <sidakveersingh8@gmail.com>
2022-09-16 12:41:36 -0400

    creating  a custom manager for the taggeditem

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F, Value, Func
 from django.db.models.aggregates import Max, Min, Count ,Avg, Sum
 from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
+
+from tags.models import TaggedItem
+
 from store.models import Collection, Product, Customer, Order, OrderItem
 
 
@@ -13,28 +17,7 @@ from store.models import Collection, Product, Customer, Order, OrderItem
 
 def say_hello(request):
 
-    # queryset = Customer.objects.all()
-    # queryset = Product.objects.filter(inventory__lt=10)
-    # queryset = Customer.objects.filter(email__icontains=".com")
-    # queryset = Collection.objects.filter(featured_product__isnull=True)
-    # queryset = Order.objects.filter(customer__id=1)
-    # queryset = Product.objects.select_related("collection").all() # for one to many relationship
-    # queryset = Product.objects.prefetch_related("promotion").all() # for many to many relationship
-    # queryset = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
-    # orders = Order.objects.aggregate(count=Count("id"))
-    # pr = OrderItem.objects.filter(product_id=1).aggregate(sum=Sum("quantity"))
-    # cu = Order.objects.filter(customer_id=1).aggregate(sum=Sum("id"))
-    # co = Product.objects.filter(collection_id=3).aggregate(max=Max("unit_price"), min=Min("unit_price"), avg=Avg("unit_price"))
-    # queryset = Customer.objects.annotate(
-    #     full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT")
-    #     )
-    queryset = Customer.objects.annotate(
-        orders_count=Count("order")
-    )
-
-    queryset = Customer.objects.annotate(amt_spent=Max())
-
+    TaggedItem.objects.get_tags_for(Product, 1)
 
     
-
     return render(request, 'hello.html', {'name': 'Mosh', "products": list(queryset)})
--- tags/models.py
+++ tags/models.py
@@ -1,6 +1,20 @@
+from calendar import c
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects\
+            .select_related("tag")\
+            .filter(
+            content_type=content_type,
+            object_id=obj_id
+            )
+
 
 
 class Tag(models.Model):
@@ -8,6 +22,7 @@ class Tag(models.Model):
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
