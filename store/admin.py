c17850e6910a81a27e14602e7c3cf072c5813e3b
Sidakveer <sidakveersingh8@gmail.com>
2022-09-24 11:47:58 -0400

    register orders table to admin site

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,10 @@
+from math import prod
 from django.contrib import admin
 from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "unit_price", "inventory_status", "inventory"]
+    list_display = ["title", "unit_price", "inventory_status", "inventory", "collection_title"]
     list_editable = ["unit_price"]
 
     @admin.display(ordering="inventory")
@@ -11,6 +12,14 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return "Low"
         return "OK" 
+    
+    def collection_title(self, product):
+        return product.collection.title
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "placed_at", "customer"]
 
 
 @admin.register(models.Customer)
@@ -21,6 +30,6 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 admin.site.register(models.Collection)
-
+    
 
 
